a093be6c36fa980d58df175fe2efbba1b7f27fa9
Darbaz Ali <darbaz.a.h@gmail.com>
2020-05-13 00:28:51 +0300

    complete repeat string algorithm

---
+++ 07_repeatString.js
@@ -0,0 +1,20 @@
+/* 
+    Repeat a String Repeat a String
+
+    Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+    
+*/
+
+const repeatStringNumTimes = (str, num) => {
+    let newStr = '';
+    for ( let i = 0; i < num; i++) {
+      newStr += str
+    }
+
+    return newStr;
+}
+
+console.log(repeatStringNumTimes('abc', 3));
+console.log(repeatStringNumTimes('*', 8));
+console.log(repeatStringNumTimes('*', -8));
+
